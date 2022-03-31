de492ac1dba5c76d91f71aa7738cf22f5b06e0fd
Brentopc <97275092+brentopc@users.noreply.github.com>
2022-03-31 19:18:36 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # RadarDetector by Brentopc
 
-Requires Miggens Scripts Framework https://miggensscripts.tebex.io/
+## Resource Description
+RadarDetector is a resource that adds a radar dector to vehicles.  It works by using the wk_wars2x radar script to detect Officers running radar.  The script has configurable K Band and Ka Band.  Ka Band is short range but 100% accurate while K Band is long range but has false alarms that are configurable.
 
-# Resource Description
+## Instructions on how to Install
+Ensure that the latest version of wk_wars2x is installed.
 
-RadarDetector is a resource that adds a radar dector to vehicles.  It works by using the wk_wars2x radar script to detect Officers running radar.  The script has configurable K-Band and Ka-Band.  Ka-Band is short range but 100% accurate while K-Band is long range but has false alarm locaions that are configurable.
+If using ESX, add the configured item to your `items` table
 
-## How to Install
-
-1. Drag the resource into the `[MiggensScripts]/MiggensScripts/Resources` folder.
-2. Edit the `config` file and set `Config.ScriptEnabled` to true.
-3. Rename and remove `UPDATED` from the `config` file
-4. Ensure that the latest version of `wk_wars2x` is installed.
-5. Modify the `wk_wars2x/cl_radar.lua` file and add 
-6. If using ESX, add the configured item to your `items` table, and set `UseESX` to true.
-7. If using mysql-async alone or with ESX, alter the `owned_vehicles` table to store vehicle accessories by running this query.
+If using mysql-async alone or with ESX, alter the `owned_vehicles` table to store vehicle accessories by running this query.
 ```ALTER TABLE `owned_vehicles` ADD `accessories` LONGTEXT NULL DEFAULT NULL```
--- server_radardetector.lua
+++ server_radardetector.lua
@@ -1,7 +1,7 @@
 --RadarDetector by Brentopc
---Version v0.9.1
+--Version 0.9.2
 
-CreateThread(function() GetConfig("VehicleAccessories", function(ResourceConfig)
+CreateThread(function() GetConfig("RadarDetector", function(ResourceConfig)
 	if ResourceConfig and ResourceConfig.ScriptEnabled then	
 	
 		local KBandLocations = ResourceConfig.KBandFakeLocations
@@ -13,8 +13,8 @@ CreateThread(function() GetConfig("VehicleAccessories", function(ResourceConfig)
 			Citizen.Wait(100)
 		end
 		
-		RegisterNetEvent('VehicleAccessories:Server:SendRadarLocation')
-		AddEventHandler('VehicleAccessories:Server:SendRadarLocation', function(playerPosition)
+		RegisterNetEvent('RadarDetector:Server:SendRadarLocation')
+		AddEventHandler('RadarDetector:Server:SendRadarLocation', function(source, playerPosition)
 			local _source = tonumber(source)
 			local playerName = GetPlayerName(_source)
 			
@@ -22,15 +22,25 @@ CreateThread(function() GetConfig("VehicleAccessories", function(ResourceConfig)
 			KaBandLocations[playerName] = playerPosition
 		end)
 		
-		RegisterCallback("VehicleAccessories:Server:isNearRadar", function(playerPosition)	
+		RegisterCallback("RadarDetector:Server:isNearRadar", function(source, playerPosition)
+			local _source = tonumber(source)
+			
 			if ResourceConfig.KBandRange > 0 then
+				local closestDistance = ResourceConfig.KBandRange
+				
 				for i, KBandLocation in pairs(KBandLocations) do				
-					if KBandLocation ~= nil then
+					if KBandLocation ~= nil then						
 						local distance = math.abs(#(KBandLocation - playerPosition))
-						if distance < ResourceConfig.KBandRange and distance > 0 then
-							return true, distance, ResourceConfig.KBandRange
+						--LogDebug(ResourceConfig.ScriptName, KBandLocation .. " - " .. playerPosition .. " = " ..distance)
+						if distance < closestDistance then
+							closestDistance = distance
 						end
 					end
+				end
+				--LogDebug(ResourceConfig.ScriptName, "Is near radar, radar is " .. closestDistance .. " away")
+				
+				if closestDistance < ResourceConfig.KBandRange and closestDistance > 0 then
+					return true, closestDistance, ResourceConfig.KBandRange
 				end
 			end
 			
@@ -57,14 +67,11 @@ CreateThread(function() GetConfig("VehicleAccessories", function(ResourceConfig)
 			for _, playerId in ipairs(GetPlayers()) do
 				local playerId = tonumber(playerId)
 				local playerName = GetPlayerName(playerId)
-				local isRunningRadar, playerPosition = TriggerCallback("VehicleAccessories:Client:isRunningRadar", playerId)
+				local isRunningRadar, playerPosition = TriggerCallback("RadarDetector:Client:isRunningRadar", playerId)
 
 				if isRunningRadar then
 					KBandLocations[playerName] = playerPosition
 					KaBandLocations[playerName] = playerPosition
-				else
-					KBandLocations[playerName] = nil
-					KaBandLocations[playerName] = nil
 				end
 			end			
 		end
@@ -72,4 +79,5 @@ CreateThread(function() GetConfig("VehicleAccessories", function(ResourceConfig)
 	else
 		LogError("Could not get config for "..ResourceConfig.ScriptName.." or script is disabled")	
 	end	
+	
 end) end)
